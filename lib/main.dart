a21830faecda0c9485e55dcc7cf28d9f40163f61
CAMILA_RODRIGUEZ_RUIZ <a.22308051281300@cbtis128.edu.mx>
2025-03-12 19:38:58 +0000

    .

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,101 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+class Sucursal {
+  final int idSucursal;
+  final String nombre;
+  final String direccion;
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+  Sucursal({
+    required this.idSucursal,
+    required this.nombre,
+    required this.direccion,
+  });
 }
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
+class SucursalListView extends StatelessWidget {
+  final List<Sucursal> sucursales = [
+    Sucursal(
+        idSucursal: 1,
+        nombre: 'Sucursal Centro',
+        direccion: 'Calle Principal 123'),
+    Sucursal(
+        idSucursal: 2,
+        nombre: 'Sucursal Norte',
+        direccion: 'Avenida Secundaria 456'),
+    Sucursal(
+        idSucursal: 3, nombre: 'Sucursal Sur', direccion: 'Calle Tercera 789'),
+    Sucursal(
+        idSucursal: 4,
+        nombre: 'Sucursal Este',
+        direccion: 'Avenida Cuarta 101'),
+    Sucursal(
+        idSucursal: 5, nombre: 'Sucursal Oeste', direccion: 'Calle Quinta 202'),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
+        title: Text('Camila Rodriguez 1300'),
+        backgroundColor: Color(0xfffdb2d5),
       ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
-        ),
+      body: ListView.separated(
+        itemCount: sucursales.length,
+        itemBuilder: (context, index) {
+          return Card(
+            elevation: 5,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
+            color: _getColor(index),
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'ID: ${sucursales[index].idSucursal}',
+                    style: TextStyle(fontSize: 14, color: Colors.grey[600]),
+                  ),
+                  SizedBox(height: 4),
+                  Text(
+                    'Nombre: ${sucursales[index].nombre}', // Mostrar "Nombre:" antes del nombre
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(height: 8),
+                  Text('Dirección: ${sucursales[index].direccion}'),
+                ],
+              ),
+            ),
+          );
+        },
+        separatorBuilder: (context, index) => SizedBox(height: 8),
       ),
+    );
+  }
+
+  Color _getColor(int index) {
+    final colors = [
+      Colors.blue[100],
+      Colors.green[100],
+      Colors.orange[100],
+      Colors.purple[100],
+      Colors.teal[100],
+    ];
+    return colors[index % colors.length]!;
+  }
+}
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SucursalListView(),
     );
   }
 }
